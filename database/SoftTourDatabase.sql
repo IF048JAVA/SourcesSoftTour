c6a7607fdba3a1b02f77b1abc93040ea5d2b6d92
AndriySV <A.S.Elips@gmail.com>
2014-09-04 19:35:40 +0300

    Add data to SoftTourDatabase.sql

---
+++ database/SoftTourDatabase.sql
@@ -0,0 +1,357 @@
+CREATE DATABASE  IF NOT EXISTS `softtour` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `softtour`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+--
+-- Host: 127.0.0.1    Database: softtour
+-- ------------------------------------------------------
+-- Server version	5.6.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `country`
+--
+
+DROP TABLE IF EXISTS `country`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `country` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `country`
+--
+
+LOCK TABLES `country` WRITE;
+/*!40000 ALTER TABLE `country` DISABLE KEYS */;
+INSERT INTO `country` VALUES (1,'Egypt'),(2,'Bulgaria'),(3,'Greece'),(4,'Turkey');
+/*!40000 ALTER TABLE `country` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `favorite`
+--
+
+DROP TABLE IF EXISTS `favorite`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `favorite` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `tour_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Favorite_User1_idx` (`user_id`),
+  KEY `fk_Favorite_TourReviw1_idx` (`tour_id`),
+  CONSTRAINT `fk_Favorite_User1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Favorite_TourReviw1` FOREIGN KEY (`tour_id`) REFERENCES `tour` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `favorite`
+--
+
+LOCK TABLES `favorite` WRITE;
+/*!40000 ALTER TABLE `favorite` DISABLE KEYS */;
+INSERT INTO `favorite` VALUES (1,'2014-09-04',2,2),(2,'2014-09-04',1,1);
+/*!40000 ALTER TABLE `favorite` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `feedback`
+--
+
+DROP TABLE IF EXISTS `feedback`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `feedback` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `cleanliness` int(1) NOT NULL,
+  `comfort` int(1) NOT NULL,
+  `location` int(1) NOT NULL,
+  `valueForMoney` int(1) NOT NULL,
+  `comment` text,
+  `hotel_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Feedback_Hotel1_idx` (`hotel_id`),
+  KEY `fk_Feedback_User1_idx` (`user_id`),
+  CONSTRAINT `fk_Feedback_Hotel1` FOREIGN KEY (`hotel_id`) REFERENCES `hotel` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Feedback_User1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `feedback`
+--
+
+LOCK TABLES `feedback` WRITE;
+/*!40000 ALTER TABLE `feedback` DISABLE KEYS */;
+INSERT INTO `feedback` VALUES (1,4,3,4,5,'Not bed',2,2),(2,5,4,5,4,NULL,3,1);
+/*!40000 ALTER TABLE `feedback` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `food`
+--
+
+DROP TABLE IF EXISTS `food`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `food` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `food`
+--
+
+LOCK TABLES `food` WRITE;
+/*!40000 ALTER TABLE `food` DISABLE KEYS */;
+INSERT INTO `food` VALUES (1,'HB'),(2,'BB'),(3,'FB'),(4,'AI'),(5,'UAI'),(6,'RO');
+/*!40000 ALTER TABLE `food` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `historyrecords`
+--
+
+DROP TABLE IF EXISTS `historyrecords`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `historyrecords` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `tour_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_HistoryReview_User1_idx` (`user_id`),
+  KEY `fk_HistoryReview_TourReviw1_idx` (`tour_id`),
+  CONSTRAINT `fk_HistoryReview_User1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_HistoryReview_TourReviw1` FOREIGN KEY (`tour_id`) REFERENCES `tour` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `historyrecords`
+--
+
+LOCK TABLES `historyrecords` WRITE;
+/*!40000 ALTER TABLE `historyrecords` DISABLE KEYS */;
+INSERT INTO `historyrecords` VALUES (1,'2014-09-04',1,2),(2,'2014-09-04',2,1);
+/*!40000 ALTER TABLE `historyrecords` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `historyrequest`
+--
+
+DROP TABLE IF EXISTS `historyrequest`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `historyrequest` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `dateFrom` date NOT NULL,
+  `dateTo` date NOT NULL,
+  `daysFrom` int(2) NOT NULL,
+  `daysTo` int(2) NOT NULL,
+  `stars` set('2','3','4','5') NOT NULL,
+  `adults` int(1) NOT NULL DEFAULT '1',
+  `children` int(1) NOT NULL DEFAULT '0',
+  `priceFrom` decimal(7,2) NOT NULL DEFAULT '0.00',
+  `priceTo` decimal(7,2) NOT NULL DEFAULT '99000.00',
+  `cityFrom` varchar(45) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `country_id` bigint(20) NOT NULL,
+  `requestDate` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_HistoryRequest_User_idx` (`user_id`),
+  KEY `fk_HistoryRequest_Country1_idx` (`country_id`),
+  CONSTRAINT `fk_HistoryRequest_User` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_HistoryRequest_Country1` FOREIGN KEY (`country_id`) REFERENCES `country` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `historyrequest`
+--
+
+LOCK TABLES `historyrequest` WRITE;
+/*!40000 ALTER TABLE `historyrequest` DISABLE KEYS */;
+INSERT INTO `historyrequest` VALUES (1,'2014-09-08','2014-09-11',6,14,'5',1,0,200.00,550.00,'lviv',1,1,'2014-09-04'),(2,'2014-09-09','2014-09-14',7,16,'4,5',1,0,150.00,400.00,'lviv',2,1,'2014-09-04');
+/*!40000 ALTER TABLE `historyrequest` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hotel`
+--
+
+DROP TABLE IF EXISTS `hotel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hotel` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `stars` int(1) NOT NULL,
+  `region_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Hotel_Region1_idx` (`region_id`),
+  CONSTRAINT `fk_Hotel_Region1` FOREIGN KEY (`region_id`) REFERENCES `region` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hotel`
+--
+
+LOCK TABLES `hotel` WRITE;
+/*!40000 ALTER TABLE `hotel` DISABLE KEYS */;
+INSERT INTO `hotel` VALUES (1,'Antalya',5,1),(2,'Viking Club',4,2),(3,'Amarilia',5,3);
+/*!40000 ALTER TABLE `hotel` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `region`
+--
+
+DROP TABLE IF EXISTS `region`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `region` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `country_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Region_Country1_idx` (`country_id`),
+  CONSTRAINT `fk_Region_Country1` FOREIGN KEY (`country_id`) REFERENCES `country` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `region`
+--
+
+LOCK TABLES `region` WRITE;
+/*!40000 ALTER TABLE `region` DISABLE KEYS */;
+INSERT INTO `region` VALUES (1,'Antalya',4),(2,'Sharm El Sheikh',1),(3,'Athens',3);
+/*!40000 ALTER TABLE `region` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'admin'),(2,'ordinaryUser'),(3,'registeredUser');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tour`
+--
+
+DROP TABLE IF EXISTS `tour`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tour` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `days` int(11) NOT NULL,
+  `departureTime` time NOT NULL,
+  `departureCity` varchar(45) NOT NULL,
+  `price` decimal(7,2) NOT NULL,
+  `hotel_id` bigint(20) NOT NULL,
+  `food_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_TourReviw_Hotel1_idx` (`hotel_id`),
+  KEY `fk_Tour_Food1_idx` (`food_id`),
+  CONSTRAINT `fk_TourReviw_Hotel1` FOREIGN KEY (`hotel_id`) REFERENCES `hotel` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Tour_Food1` FOREIGN KEY (`food_id`) REFERENCES `food` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tour`
+--
+
+LOCK TABLES `tour` WRITE;
+/*!40000 ALTER TABLE `tour` DISABLE KEYS */;
+INSERT INTO `tour` VALUES (1,'2014-09-14',6,'11:30:00','kyiv',242.00,3,4),(2,'2014-09-19',10,'16:00:00','lviv',350.00,2,3);
+/*!40000 ALTER TABLE `tour` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `e-mail` varchar(30) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `birthday` date NOT NULL,
+  `age` tinyint(3) unsigned NOT NULL,
+  `sex` enum('MALE','FEMALE') NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `role_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_User_Role1_idx` (`role_id`),
+  CONSTRAINT `fk_User_Role1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Andriy','elips@gmail.com','1111','1987-11-24',26,'MALE','+380958000877',3),(2,'Taras','taras@gmail.com','2222','1990-06-12',24,'MALE','+380994455666',3);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-09-04 16:47:03
